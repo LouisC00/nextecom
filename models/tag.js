b1933300ddf6a5afa060d10d7cf5fc551b1d8150
Louis <andrewng23@proton.me>
2024-02-26 16:07:21 -0700

    add import other model for solving future conflict

--- models/tag.js
+++ models/tag.js
@@ -1,5 +1,7 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 import uniqueValidator from "mongoose-unique-validator";
+import Category from "@/models/category";
+
 const tagSchema = new mongoose.Schema(
   {
     name: {
