75430be07b67bfd86d9b516c755f35e4278e429e
Louis <andrewng23@proton.me>
2024-03-01 17:47:28 -0700

    init

---
+++ app/dashboard/user/stripe/success/page.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function UserStripeSuccess() {
+  return (
+    <div className="container my-5">
+      <div className="row">
+        <div className="col text-center">
+          <p>
+            Thank you for your purchase. You can now check your order status in
+            the dashboard
+          </p>
+          <hr />
+          <Link
+            className="btn btn-primary btn-raised"
+            href="/dashboard/user/orders"
+          >
+            View order status
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
