73941a503029be7f6fecde9d02419526d2e6c568
Louis <andrewng23@proton.me>
2024-03-21 14:57:45 -0600

    cancel fetching the latest product with new status

--- app/api/admin/orders/[orderId]/route.js
+++ app/api/admin/orders/[orderId]/route.js
@@ -20,15 +20,13 @@ export async function PUT(req, context) {
       );
     }
 
-    const updatedOrder = await Order.findByIdAndUpdate(
-      context.params.orderId,
-      {
-        delivery_status: body.delivery_status,
-      },
-      { new: true }
-    );
+    // Update the order status without fetching the updated order
+    await Order.findByIdAndUpdate(context.params.orderId, {
+      delivery_status: body.delivery_status,
+    });
 
-    return NextResponse.json(updatedOrder);
+    // Return a success message
+    return NextResponse.json({ message: "Order status updated successfully" });
   } catch (err) {
     console.log(err);
     return NextResponse.json(
@@ -37,3 +35,39 @@ export async function PUT(req, context) {
     );
   }
 }
+
+// export async function PUT(req, context) {
+//   await dbConnect();
+//   const body = await req.json();
+
+//   try {
+//     const order = await Order.findById(context.params.orderId);
+
+//     if (!order) {
+//       return NextResponse.json({ err: "Order not found" }, { status: 404 });
+//     }
+
+//     if (order.refunded) {
+//       return NextResponse.json(
+//         { err: "Cannot update delivery status for a refunded order" },
+//         { status: 400 }
+//       );
+//     }
+
+//     const updatedOrder = await Order.findByIdAndUpdate(
+//       context.params.orderId,
+//       {
+//         delivery_status: body.delivery_status,
+//       },
+//       { new: true }
+//     );
+
+//     return NextResponse.json(updatedOrder);
+//   } catch (err) {
+//     console.log(err);
+//     return NextResponse.json(
+//       { err: "Server error. Please try again." },
+//       { status: 500 }
+//     );
+//   }
+// }
