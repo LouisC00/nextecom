5af7e1fa0ee7d97057d9c1275d9101f5ae88a7a0
Louis <andrewng23@proton.me>
2024-03-21 15:20:38 -0600

    add price filter that more than

--- utils/filterData.js
+++ utils/filterData.js
@@ -4,5 +4,6 @@ export const priceRanges = [
   { min: 50, max: 100, label: "$50 - $100" },
   { min: 100, max: 200, label: "$100 - $200" },
   { min: 200, max: 500, label: "$200 - $500" },
-  { min: 500, max: 900, label: "$500 - $900" },
+  { min: 500, max: 1000, label: "$500 - $1000" },
+  { min: 1000, max: 10000, label: "$1000+" },
 ];
